5fb850edf349436d41aabbec469ade81e7bbeb46
Dani <dani@pop-os.localdomain>
2021-10-21 17:14:38 +0200

    Fixed crate errors, POST and GET working

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,16 @@
-
-#[macro_use]            // seems to be useless idk why
-extern crate rocket;
-extern crate dashmap;
-extern crate rand;
-
-
-/************************************************************
- * Pointless use section, idk why 'extern crate' won't work *
- * Must fix ASAP because this looks so awful                *
- ************************************************************/
-
-// dashmap
 use dashmap::DashMap;
-
-// rocket
-use rocket::response::status::BadRequest;
-use rocket::State;
-use rocket::response::Redirect;
-use rocket::response::status::NotFound;
-
-// rand
-use rand::thread_rng;
-use crate::rand::Rng;
+use rand::{thread_rng, Rng};
+use rocket::{
+    response::{
+        status::{BadRequest, NotFound},
+        Redirect,
+    },
+    State,
+};
 
 
-/****************************
- *  Actual code starts here *
- ****************************/
-
-
-#[get("/")]                 // test
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-
-#[launch]
-fn rocket() -> _ {          // sharing dashmap state across threads
-    rocket::build()
-        .manage(DashMap::<u32, String>::new())
-        .mount("/", routes![index])
-}
+#[macro_use]
+extern crate rocket;
 
 
 #[post("/api/shorten?<url>")]       // listen to POST requests of the form /api/shorten?url=___
@@ -61,4 +31,12 @@ fn redirect(key: u32, state: &State<DashMap<u32, String>>) -> Result<Redirect, N
         .get(&key)
         .map(|url| Redirect::to(url.clone()))
         .ok_or(NotFound("Invalid or expired link"))
+}
+
+
+#[launch]           // manage dashmap state across threads
+fn rocket() -> _ {
+    rocket::build()
+        .manage(DashMap::<u32, String>::new())
+        .mount("/", routes![shorten, redirect])
 }
