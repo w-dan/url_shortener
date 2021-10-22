47350a69ff10feea445f8277a157b25eeca6286d
Dani <dani@pop-os.localdomain>
2021-10-22 13:14:43 +0200

    Added tests

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,70 @@ fn redirect(key: u32, state: &State<DashMap<u32, String>>) -> Result<Redirect, N
 }
 
 
-#[launch]           // manage dashmap state across threads
+#[launch]                           // manage dashmap state across threads
 fn rocket() -> _ {
     rocket::build()
         .manage(DashMap::<u32, String>::new())
         .mount("/", routes![shorten, redirect])
 }
+
+
+
+/*****************************************
+ *               T E S T S               *
+ *****************************************/
+
+
+#[cfg(test)]
+mod tests {
+    use rocket::{http::Status, local::blocking::Client};
+    use super::rocket;
+
+    #[test]
+    fn valid_requests() {           // valid request        
+        let client = Client::tracked(rocket())
+            .expect("valid rocket instance");
+
+        let response = client
+            .post("/api/shorten?url=https://duck.com")
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        
+        let key: u32 = response
+            .into_string()
+            .expect("body")
+            .parse()
+            .expect("valid u32");
+
+        let response = client.get(format!("/{}", key)).dispatch();
+
+        assert_eq!(response.status(), Status::SeeOther);
+
+        let redirect = response
+            .headers()
+            .get_one("Location")
+            .expect("location header");
+
+        assert_eq!(redirect, "https://duck.com")
+    }
+
+    #[test]
+    fn empty_url() {                // bad request on missing url parameter
+        let client = Client::tracked(rocket())
+            .expect("valid rocket instance");
+
+        let response = client.post("/api/shorten?url=").dispatch();
+        assert_eq!(response.status(), Status::BadRequest);
+    }
+
+    #[test]
+    fn invalid_url() {              // not found on invalid shortened url
+        let client = Client::tracked(rocket())
+            .expect("valid rocket instance");
+    
+        let response = client.post("/123").dispatch();
+        assert_eq!(response.status(), Status::NotFound);
+    }
+
+}
+
